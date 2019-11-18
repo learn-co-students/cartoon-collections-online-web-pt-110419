2d9741321e2976b739385ad87d9a16a12f5f72d4
Oksana Nichols <oksanahnichols@yahoo.com>
2019-11-18 07:10:41 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,3 +14,19 @@ def find_the_cheese# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
+describe 'Cartoon Collections' do
+  describe '#roll_call_dwarves' do
+    it 'prints out the 7 dwarfs in a numbered list' do
+      dwarves = %w[Dopey Grumpy Bashful]
+
+      output = capture_stdout do
+        roll_call_dwarves(dwarves)
+      end
+
+      # This regex allows for arbitrary characters between
+      # the numbering and the name
+      expect(output).to match(/1.*Dopey/)
+      expect(output).to match(/2.*Grumpy/)
+      expect(output).to match(/3.*Bashful/)
+    end
+  end
